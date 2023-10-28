aabef4dc936274e73a98b0afb14c9393455d3732
David De Anda <omarandstuff@gmail.com>
2023-10-27 20:25:43 -0600

    Test using testing tools

--- tests/ExpressSession.module.test.ts
+++ tests/ExpressSession.module.test.ts
@@ -1,13 +1,22 @@
-import { Logger } from '@universal-packages/logger'
+import { initialize } from '@universal-packages/express-controllers-session'
 
 import { ExpressSessionModule } from '../src'
 
+jest.mock('@universal-packages/express-controllers-session')
+
+jestCore.runBare({
+  coreConfigOverride: {
+    config: { location: './tests/__fixtures__/config' },
+    modules: { location: './tests/__fixtures__' },
+    logger: { silence: true }
+  }
+})
+
 describe(ExpressSessionModule, (): void => {
   it('behaves as expected', async (): Promise<void> => {
-    const logger = new Logger({ silence: true })
-    const module = new ExpressSessionModule({} as any, logger)
+    expect(core.coreModules.expressSessionModule).not.toBeUndefined()
+    expect(core.coreModules.expressSessionModule.config).toEqual({ registryId: 'my-app' })
 
-    await module.prepare()
-    await module.release()
+    expect(initialize).toHaveBeenCalledWith({ registryId: 'my-app' })
   })
 })
---
+++ tests/__fixtures__/ExpressSession.task.ts
@@ -0,0 +1,1 @@
+export { default } from '../../src/ExpressSession.universal-core-task'
