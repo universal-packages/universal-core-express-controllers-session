a2396b2817c3c1d9f85ed001a3816be136fdd0c3
David De Anda <omarandstuff@gmail.com>
2024-07-08 19:29:09 -0600

    Initializer in favor of custom task finishing

--- src/ExpressControllersSession.universal-core-initializer.ts
+++ src/ExpressControllersSession.universal-core-initializer.ts
@@ -2,6 +2,7 @@ import CoreInitializer from '@universal-packages/core/CoreInitializer'
 
 export default class ExpressControllersSessionInitializer extends CoreInitializer {
   public static readonly initializerName = 'express-controllers-session'
+  public static readonly description: string = 'Express Controllers Session initializer'
 
   public readonly templatesLocation: string = `${__dirname}/templates`
 }
